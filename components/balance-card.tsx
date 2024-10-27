5a577917704ff8432c057b056f3b7135b5f5d57c
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2024-10-26 23:17:23 -0300

    Fix balance-card:<Se sube el drop-shadown correcto>

--- components/balance-card.tsx
+++ components/balance-card.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function balanceCard() {
     return (
-        <section className='bg-gradient-to-r from-blue-gradient via-purple-gradient via-53% to-orange-gradient to-92% px-3.5 pt-3 pb-5 rounded-2xl text-white flex flex-col items-center gap-8 drop-shadow-sm'>
+        <section className='bg-gradient-to-r from-blue-gradient via-purple-gradient via-53% to-orange-gradient to-92% px-3.5 pt-3 pb-5 rounded-2xl text-white flex flex-col items-center gap-8 drop-shadow-lg'>
             <article className='flex flex-col items-center gap-2.5'>
                 <h2 className='text-xs font-semibold'>1 Jun. 2024 - 4 Jun. 2024</h2>
                 <h1 className='font-semibold text-2xl'>$41.000,00</h1>
