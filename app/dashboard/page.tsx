33927e740ba2f84395546dab9cd23f0d67645732
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2024-10-27 14:03:27 -0300

    Fix Dashboard:<Habia unas props que se habian eliminado pero seguian siendo utilizadas>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,9 +5,6 @@ function page() {
     return (
         <main className='flex flex-col gap-9'>
             <BalanceCard
-                initialDate='1 Jun. 2024'
-                finalDate='4 Jun. 2024'
-                amount='$41.000,00'
                 className='mt-24'
             />
             <Transfers />
