22de6e707b572b4238988cd6bccd2c1933c60e7d
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2024-10-27 04:20:34 -0300

    Fix nextversion:<se hace downgrade a la version 14.2.15 de next para tener compatibilidad con otros paquetes>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#DB3535",
-        secondary: "#379137",
-        "light-gray": "#ABABAB",
-        "dark-gray": "#525252",
-        "custom-white": "#F1F2F6",
-        "blue-gradient": "#4F80C3",
-        "purple-gradient": "#C262EA",
-        "pink-gradient": "#D56DC4",
-        "orange-gradient": "#E67B99",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#DB3535",
+				secondary: "#379137",
+				"light-gray": "#ABABAB",
+				"dark-gray": "#525252",
+				"custom-white": "#F1F2F6",
+				"blue-gradient": "#4F80C3",
+				"purple-gradient": "#C262EA",
+				"pink-gradient": "#D56DC4",
+				"orange-gradient": "#E67B99",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
