7056ab97fb798a7a9308c90edb1d608dd33ddae3
Lautaro Roldan <lautaronicolasroldan@gmail.com>
2024-10-31 16:38:39 -0300

    Fix .env:<Se elimina y se cambia una variable de entorno, ya que no estaba colocada en el .env>

--- app/db/client.ts
+++ app/db/client.ts
@@ -4,12 +4,13 @@ import { users } from './schema/user';
 import { transferTable } from './schema/transfer';
 
 const client = createClient({
-    url: process.env.NEXT_PUBLIC_DATABASE_URL!,
-    authToken: process.env.NEXT_PUBLIC_DATABASE_API_KEY!
+    url: process.env.NEXT_PUBLIC_TURSO_DATABASE_URL!,
+    authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN!
 })
 
-export const db = drizzle({ client, schema: {
-    users,
+export const db = drizzle({
+    client, schema: {
+        users,
         transferTable
     }
 });
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
@@ -5,7 +6,7 @@ export default defineConfig({
     schema: './app/db/schema/*',
     out: './drizzle',
     dbCredentials: {
-        url: "libsql://balanticco-lautaroroldan.turso.io",
-        authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzAyMjQ1MDksImlkIjoiYTI5ZTgzZDItYjVlOC00MDM5LWIwNjgtYzAxOTc0OWZmOTZiIn0.ePbKZ9WiU8CPGMpmjq5l7X4lEYBZaFmVvyzYvDy5voDvFEqq8zN0J-2Tnon9QPxgrf4t586A0xF7ZGCbuWEHAA"
+        url: process.env.NEXT_PUBLIC_TURSO_DATABASE_URL!,
+        authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN!
     }
 })
